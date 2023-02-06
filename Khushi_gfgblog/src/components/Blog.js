c380f261aac3f65f3f0e7c4d15eefdd3471ebf01
raj narayan mishra <77186976+raj-ravan@users.noreply.github.com>
2023-02-06 23:18:57 +0530

    R

---
+++ Khushi_gfgblog/src/components/Blog.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+
+export const Blog = () => {
+  return (
+    <>
+      <div className="row " >
+        <div className="col-sm-4 mb-3 mb-sm-4"  >
+          <div className="card" style={{fontfamily:"Montserrat", position:"relative",left:170,top:70,height:180,width:516}} >
+            <div className="card-body"  >
+              <h5 className="card-title"><b> Will Web3 Technology will have an impact in coming years?</b></h5>
+              <a
+                href="/"
+                className="btn btn-sm"
+                style={{fontfamily:"Google Sans", background: "green", position:"relative",left:0,color:"white",top:50}}
+              >
+                Read more
+              </a>
+
+              <object align="right"> 
+              <small className="text-muted date1" >
+               24 January 23
+              </small>
+              </object>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-4 mb-3 mb-sm-4" >
+          <div className="card" style={{fontfamily:"Montserrat",position:"relative",left:250,top:70,height:180,width:516}}>
+            <div className="card-body">
+              <h5 className="card-title"><b>Will Web3 Technology will have an impact in coming years?</b></h5>
+              <a
+                href="/"
+                className="btn btn-sm"
+                style={{ fontfamily:"Google Sans",background: "green", position:"relative",left:0,color:"white",top:50 }}
+              >
+                Read more
+              </a>
+
+              <object align="right"> 
+              <small className="text-muted date1" >
+               24 January 23
+              </small>
+              </object>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4 mb-3 mb-sm-4">
+          <div className="card" style={{fontfamily:"Montserrat",position:"relative",left:170,top:70,height:180,width:516}}>
+            <div className="card-body">
+              <h5 className="card-title"><b>Will Web3 Technology will have an impact in coming years?</b></h5>
+              <a
+                href="/"
+                className="btn btn-sm"
+                style={{ fontfamily:"Google Sans", background: "green", position:"relative",left:0,top:55,color:"white"}}
+              >
+                Read more
+              </a>
+
+              <object align="right"> 
+              <small className="text-muted date" >
+               24 January 23
+              </small>
+              </object>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-4 mb-3 mb-sm-4" >
+          <div className="card" style={{fontfamily:"Montserrat",position:"relative",left:250,top:70,height:180,width:516}}>
+            <div className="card-body">
+              <h5 className="card-title"><b>Will Web3 Technology will have an impact in coming years?</b></h5>
+              <a
+                href="/"
+                className="btn btn-sm"
+                style={{ fontfamily:"Google Sans",background: "green", position:"relative",left:0,color:"white",top:55 }}>
+                Read more
+              </a>
+              <object align="right"> 
+              <small className="text-muted date"  >
+               24 January 23
+              </small>
+              </object>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
