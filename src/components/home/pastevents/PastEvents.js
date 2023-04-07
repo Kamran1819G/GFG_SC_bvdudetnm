ae695f4d51e67b92a2a6ba1d34bc954db8ff88a0
Kamran Khan <kamrankhan.sde@gmail.com>
2023-04-07 13:39:22 +0530

    Mobile View added

---
+++ src/components/home/pastevents/PastEvents.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./PastEvents.css";
+import { Carousel } from "3d-react-carousal";
+import autorun from "../../../assets/Events/autorun.png";
+import dsa from "../../../assets/Events/dsa.jpeg";
+import flutter from "../../../assets/Events/flutter_event.png";
+import ingauration from "../../../assets/Events/Inaugration.png";
+import Newton from "../../../assets/Events/NS_CC.png";
+import hacktober from "../../../assets/Events/HacktoberFest 2022.png";
+import AutoFinal from "../../../assets/Events/Auto run final round.png";
+// npm install 3d-react-carousal --force
+
+const PastEvents = () => {
+  let slides = [
+    <img src={ingauration} style={{ height:"350px", width: "350px" }} alt="7" />,
+    <img src={dsa} style={{ height:"350px", width: "350px" }} alt="6" />,
+    <img src={hacktober} style={{ height:"350px", width: "350px" }} alt="5" />,
+    <img src={Newton} style={{ height:"350px", width: "350px" }} alt="4" />,
+    <img src={autorun} style={{ height:"350px", width: "350px" }} alt="3" />,
+    <img src={AutoFinal} style={{ height:"350px", width: "350px" }} alt="2" />,
+    <img src={flutter} style={{ height:"350px", width: "350px" }} alt="1" />,
+  ];
+
+  const callback = function (index) {
+    console.log("callback", index);
+  };
+
+  return (
+    <section className="section h-100vh" id="pastevent">
+      <div className="container">
+        <div className="gbc2"></div>
+        <div className="row">
+          <div className="text-center">
+            <h2 className="section-title">
+              Our{" "}
+              <span style={{ color: "var(--secondary-color)" }}>Past</span>{" "}
+              Events
+            </h2>
+            <p className="section-subtitle">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="carousal">
+            {" "}
+            <Carousel
+              slides={slides}
+              interval={1000}
+              onSlideChange={callback}
+            />
+          </div>
+          <link
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            rel="stylesheet"
+            integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+            crossOrigin="anonymous"
+          />          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PastEvents;
---
+++ src/components/team/TeamCarousel.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import CoreTeam from "../../assets/CoreMembers/CoreTeam.jpeg";
+import Team from "../../assets/Events/team.jpeg";
+import eventpic_1 from "../../assets/Events/event_pic2.jpg";
+import principal from "../../assets/Events/principal.jpeg";
+import principal1 from "../../assets/Events/principal1.png";
+import DS from "../../assets/Events/DS.jpg";
+import Flutter from "../../assets/Events/Flutter.jpg";
+
+function TeamCarousel() {
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-sm-5 col-lg-9 w-100vw">
+              <Carousel fade>
+                <Carousel.Item>
+                  <img src={CoreTeam} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Core Team Members ( Foundation Team )</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={Team} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Team 2022</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={principal1} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Honarable principal of BVDU DET NM</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={principal} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Honarable principal of BVDU DET NM</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={DS} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Data Structure & Algortihm Session</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={eventpic_1} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Hacktober Event</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={Flutter} className="d-block w-100" alt="..." />
+                  <Carousel.Caption>
+                    <h5>Flutter Session</h5>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default TeamCarousel;
